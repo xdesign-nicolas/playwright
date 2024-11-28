bc0ecbf184ba803b71e251756ac76312fb5205c4
xdesign-nicolas <nicolas.fernandez@xdesign.com>
2024-11-28 12:50:39 +0000

    change playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.CI ? 'https://xdesign-nicolas.github.io/playwright' : 'http://localhost:3000',
     screenshot: 'only-on-failure',
     actionTimeout: 10000,
     navigationTimeout: 10000,
