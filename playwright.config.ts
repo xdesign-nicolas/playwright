85fff255ac558136b2545d001f329da375374c9d
xdesign-nicolas <nicolas.fernandez@xdesign.com>
2024-11-28 11:09:30 +0000

    change playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,13 @@ const config: PlaywrightTestConfig = {
   use: {
     baseURL: process.env.CI ? 'https://xdesign-nicolas.github.io/playwright' : 'http://localhost:3000',
     screenshot: 'only-on-failure',
+    actionTimeout: 10000,
   },
   webServer: {
     command: 'npm start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 }
 
